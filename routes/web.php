4bf0ae0f0a85fcd49ce3287b06974352b60e814b
seyiAjibola <info2mee.seyi@gmail.com>
2026-04-21 00:52:18 +0100

    add artisan uurl

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::redirect('/', '/login');
 
@@ -11,6 +12,15 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+
+    return 'Cache cleared';
+});
 
 require __DIR__.'/admin.php';
 require __DIR__.'/auth.php';
